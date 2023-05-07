fbf1694afe4fc7a6703d7ed9c05747626fac0e0b
zzb98 <zzegaw@connect.ust.hk>
2023-05-08 01:40:55 +0800

    1

--- js/main.js
+++ js/main.js
@@ -27,69 +27,69 @@ var iUp = (function () {
 	}
 })();
 
-// $(document).ready(function () {
+$(document).ready(function () {
 
-// 	// 获取一言数据
-// 	//fetch('https://v1.hitokoto.cn').then(function (res) {
-// 	//	return res.json();
-// 	//}).then(function (e) {
-// 	//	$('#description').html(e.hitokoto + "<br/> -「<strong>" + e.from + "</strong>」")
-// 	//}).catch(function (err) {
-// 	//	console.error(err);
-// 	//})
+	// 获取一言数据
+	//fetch('https://v1.hitokoto.cn').then(function (res) {
+	//	return res.json();
+	//}).then(function (e) {
+	//	$('#description').html(e.hitokoto + "<br/> -「<strong>" + e.from + "</strong>」")
+	//}).catch(function (err) {
+	//	console.error(err);
+	//})
 
 
-// 	// var url = 'https://query.yahooapis.com/v1/public/yql' + 
-// 	// '?q=' + encodeURIComponent('select * from json where url=@url') +
-// 	// '&url=' + encodeURIComponent('https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=8') +
-// 	// '&format=json&callback=?';
+	// var url = 'https://query.yahooapis.com/v1/public/yql' + 
+	// '?q=' + encodeURIComponent('select * from json where url=@url') +
+	// '&url=' + encodeURIComponent('https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=8') +
+	// '&format=json&callback=?';
 
-// 	/**
-// 	 * 获取Bing壁纸
-// 	 * 原先 YQL 已经无法提供服务了
-// 	 * 改用 JsonBird：https://bird.ioliu.cn/
-// 	 * 
-// 	 */
-// 	var url = 'https://bird.ioliu.cn/v1/?url=https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=8';
-// 	var imgUrls = JSON.parse(sessionStorage.getItem("imgUrls"));
-// 	var index = sessionStorage.getItem("index");
-// 	var $panel = $('#panel');
-// 	if (imgUrls == null) {
-// 		imgUrls = new Array();
-// 		index = 0;
-// 		$.get(url, function (result) {
-// 			images = result.images;
-// 			for (let i = 0; i < images.length; i++) {
-// 				const item = images[i];
-// 				imgUrls.push(item.url);
-// 			}
-// 			var imgUrl = imgUrls[index];
-// 			var url = "https://www.bing.com" + imgUrl;
-// 			$panel.css("background", "url('" + url + "') center center no-repeat #666");
-// 			$panel.css("background-size", "cover");
-// 			sessionStorage.setItem("imgUrls", JSON.stringify(imgUrls));
-// 			sessionStorage.setItem("index", index);
-// 		});
-// 	} else {
-// 		if (index == 7)
-// 			index = 0;
-// 		else
-// 			index++;
-// 		var imgUrl = imgUrls[index];
-// 		var url = "https://www.bing.com" + imgUrl;
-// 		$panel.css("background", "url('" + url + "') center center no-repeat #666");
-// 		$panel.css("background-size", "cover");
-// 		sessionStorage.setItem("index", index);
-// 	}
+	/**
+	 * 获取Bing壁纸
+	 * 原先 YQL 已经无法提供服务了
+	 * 改用 JsonBird：https://bird.ioliu.cn/
+	 * 
+	 */
+	var url = 'https://bird.ioliu.cn/v1/?url=https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=8';
+	var imgUrls = JSON.parse(sessionStorage.getItem("imgUrls"));
+	var index = sessionStorage.getItem("index");
+	var $panel = $('#panel');
+	if (imgUrls == null) {
+		imgUrls = new Array();
+		index = 0;
+		$.get(url, function (result) {
+			images = result.images;
+			for (let i = 0; i < images.length; i++) {
+				const item = images[i];
+				imgUrls.push(item.url);
+			}
+			var imgUrl = imgUrls[index];
+			var url = "https://www.bing.com" + imgUrl;
+			$panel.css("background", "url('" + url + "') center center no-repeat #666");
+			$panel.css("background-size", "cover");
+			sessionStorage.setItem("imgUrls", JSON.stringify(imgUrls));
+			sessionStorage.setItem("index", index);
+		});
+	} else {
+		if (index == 7)
+			index = 0;
+		else
+			index++;
+		var imgUrl = imgUrls[index];
+		var url = "https://www.bing.com" + imgUrl;
+		$panel.css("background", "url('" + url + "') center center no-repeat #666");
+		$panel.css("background-size", "cover");
+		sessionStorage.setItem("index", index);
+	}
 
-// 	$(".iUp").each(function (i, e) {
-// 		iUp.up(e);
-// 	});
+	$(".iUp").each(function (i, e) {
+		iUp.up(e);
+	});
 
-// 	$(".js-avatar")[0].onload = function () {
-// 		$(".js-avatar").addClass("show");
-// 	}
-// });
+	$(".js-avatar")[0].onload = function () {
+		$(".js-avatar").addClass("show");
+	}
+});
 
 $('.btn-mobile-menu__icon').click(function () {
 	if ($('.navigation-wrapper').css('display') == "block") {
